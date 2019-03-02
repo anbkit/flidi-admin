8a52ac5846abbdcf407797b9cf0626915fc0495e
An Nguyen <an@fuelcloud.com>
2019-03-02 14:46:45 +0700

    Fix controller

--- routes/api.php
+++ routes/api.php
@@ -27,4 +27,4 @@ Route::get('/blog/list/{location_id}','BlogController@getList')->where(['locatio
 Route::get('/blog/posts/{user_id}','BlogController@getListByUser')->where(['user_id'=>'[0-9]+']);
 Route::get('/blog/comments/{blog_id}','BlogController@getComments')->where(['blog_id'=>'[0-9]+']);
 Route::get('/blog/likes/{blog_id}','BlogController@getLikes')->where(['blog_id'=>'[0-9]+']);
-Route::get('/place/photos/{place_id}','Placecontroller@getPhotos')->where(['place_id'=>'[0-9]+']);
+Route::get('/place/photos/{place_id}','PlaceController@getPhotos')->where(['place_id'=>'[0-9]+']);
