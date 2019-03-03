d1949c4f098b121e1c371a1f5b0e05498bd2df2a
An Nguyen <an@fuelcloud.com>
2019-03-03 21:53:44 +0700

    Fix controller

--- config/database.php
+++ config/database.php
@@ -70,7 +70,21 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-
+        'flidi2' => [
+            'driver' => 'mysql',
+            'host' => env('DB_FLIDI2_HOST', '127.0.0.1'),
+            'port' => env('DB_FLIDI2_PORT', '3306'),
+            'database' => env('DB_FLIDI2_DATABASE', 'forge'),
+            'username' => env('DB_FLIDI2_USERNAME', 'forge'),
+            'password' => env('DB_FLIDI2_PASSWORD', ''),
+            'unix_socket' => env('DB_FLIDI2_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
